37e92ae59e161cdc9cd9d87076d653ba12192437
McZen84 <macdanny84@gmail.com>
2024-06-25 13:51:20 +0200

    Update mario.c

--- problem set 0/mario-less/mario.c
+++ problem set 0/mario-less/mario.c
@@ -11,7 +11,7 @@ int main(void)
     {
         height = get_int("Height: ");
     }
-    while (height <= 0);
+    while (height <= 0 || height > 8);
 
     // Print a pyramid of that height
     for (int i = 0; i < height; i++)
