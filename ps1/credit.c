e249b8896909759a82e094c536ae3dc60832c7f9
McZen84 <macdanny84@gmail.com>
2024-06-28 18:31:56 +0200

    Update credit.c

--- ps1/credit.c
+++ ps1/credit.c
@@ -7,23 +7,23 @@ int length = 0;
 
 int main(void)
 {
-     // get creditcard number
+    // get creditcard number
     card_number = get_long("Number: ");
 
     // get length of creditcard number
     long l = card_number;
-    while(l > 0)
+    while (l > 0)
     {
         l = (l / 10);
         length++;
     }
     // check length
-    if(length != 13 && length != 15 && length != 16)
+    if (length != 13 && length != 15 && length != 16)
     {
         printf("INVALID\n");
         return 0;
     }
-// calculate checksum
+    // calculate checksum
     int last_digit, second_last_digit;
     int Sum1 = 0;
     int Sum2 = 0;
@@ -45,13 +45,13 @@ int main(void)
         int d1 = second_last_digit % 10;
         int d2 = second_last_digit / 10;
         Sum2 = Sum2 + d1 + d2;
- }
+    }
 
     while (x > 0);
     Total = Sum1 + Sum2;
 
     // check algorithm result
-    if(Total % 10 != 0)
+    if (Total % 10 != 0)
     {
         printf("INVALID\n");
         return 0;
@@ -72,19 +72,19 @@ int main(void)
 
     // check which card
     if ((length == 15) && (num == 34 || num == 37))
-        {
-            printf("AMEX\n");
-        }
+    {
+        printf("AMEX\n");
+    }
 
     else if ((length == 16) && (num >= 51 && num <= 55))
-        {
-            printf("MASTERCARD\n");
-        }
+    {
+        printf("MASTERCARD\n");
+    }
 
     else if ((length == 13 || length == 16) && (num >= 40 && num <= 49))
-        {
-            printf("VISA\n");
-        }
+    {
+        printf("VISA\n");
+    }
     else
     {
         printf("INVALID\n");
